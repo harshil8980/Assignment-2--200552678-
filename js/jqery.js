85c297d253d03fc7cc88ec120fa2be47c0c096f2
harshil8980 <81788679+harshil8980@users.noreply.github.com>
2023-07-31 20:46:10 -0400

    first commit

---
+++ js/jqery.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  var orderButton = document.getElementById("order-btn");
+  orderButton.addEventListener("click", function () {
+    var size = document.getElementById("size").value;
+    var crust = document.getElementById("crust").value;
+    var sauce = document.getElementById("sauce").value;
+    var price;
+    //Prices according to the size
+   // var sauce = document.querySelector('input[name="sauce"]:checked');
+    if(size=="small")
+    {
+      price=9.99;
+    }   
+    else if(size=="medium")
+    {
+      price=10.99;
+    }
+    else if(size=="large")
+    {
+      price=12.99;
+    }
+      alert("Order Placed Successfully (:\n Size: "+size+"\n Type of pizza: "+crust+"\n Name of the sauce: "+sauce+"\n Price is: $"+price);
+   
+    // Check if a sauce is selected
+  });
+});
